1b30f4dd7846f88bc7c8fa8f047999e95e1c4734
Timothy Manas <timothy.romero.manas@gmail.com>
2026-08-16 15:39:46 -0400

    Adding DropDown verification

--- tests/selection.spec.js
+++ tests/selection.spec.js
@@ -24,3 +24,40 @@ test("Has Selection Page", async ({ page }) => {
     await expect(genderOutput).toHaveText('other');
 })  
 
+test("Can select Checkboxes", async ({ page }) => {
+    await page.goto('https://qapracticehub.com/#selection');
+
+    const checkboxOutput = await page.getByTestId('skills-output');
+
+    await page.getByTestId('skills-checkboxes').getByText('Selenium').click();
+    await expect(checkboxOutput).toHaveText('selenium');
+    await page.getByTestId('skills-checkboxes').getByText('Playwright').click();
+    await expect(checkboxOutput).toHaveText('selenium, playwright');
+    await page.getByTestId('skills-checkboxes').getByText('Cypress').click();
+    await expect(checkboxOutput).toHaveText('selenium, playwright, cypress');
+    await page.getByTestId('skills-checkboxes').getByText('Appium').click();
+    await expect(checkboxOutput).toHaveText('selenium, playwright, cypress, appium');
+
+
+    await page.getByTestId('skills-checkboxes').getByText('Appium').click();
+    await expect(checkboxOutput).toHaveText('selenium, playwright, cypress');
+
+    await page.getByTestId('skills-checkboxes').getByText('Cypress').click();
+    await expect(checkboxOutput).toHaveText('selenium, playwright');
+
+    await page.getByTestId('skills-checkboxes').getByText('Playwright').click();
+    await expect(checkboxOutput).toHaveText('selenium');
+
+    await page.getByTestId('skills-checkboxes').getByText('Selenium').click();
+    await expect(checkboxOutput).toHaveText('None selected');
+})
+
+
+test("Can select Dropdown", async ({ page }) => {
+    await page.goto('https://qapracticehub.com/#selection');
+
+    await page.getByTestId('country-select').click();
+    await page.getByTestId('country-select').selectOption('germany');
+    await expect(page.getByTestId('country-output')).toHaveText('Germany');
+
+})
